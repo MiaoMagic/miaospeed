9ccd3dc9db18c47ddcb8193f860c061db9d332bb
MoShaoLi <moshaoli688@gmail.com>
2024-01-03 15:43:13 +0800

    Revert "Modify the clash library and update dependencies"
    This reverts commit 54eaefc155480d34f37145ae18031c20311c2d85.

--- vendors/clash/metadata_core.go
+++ vendors/clash/metadata_core.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/moshaoli688/clash/constant"
+	"github.com/Dreamacro/clash/constant"
 )
 
 func urlToMetadata(rawURL string, network constant.NetWork) (addr constant.Metadata, err error) {
